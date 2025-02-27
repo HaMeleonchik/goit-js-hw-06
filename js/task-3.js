e1b8b40110b421a2fe06e509c298c33d61c7cf7e
HaMeleonchik <tntshow.tiktok@gmail.com>
2025-02-27 12:03:28 +0200

    hw6

--- js/task-3.js
+++ js/task-3.js
@@ -1,56 +1,32 @@
+class StringBuilder {
+#value 
 
-// Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
+constructor (initialValue){
+this.#value = initialValue
+}
+getValue(){
+ return this.#value;
+}
+    padEnd(str) {
+    this.#value = this.#value + str ;
+   }
+   padStart(str){
+    this.#value = str + this.#value;
+   }
+   padBoth(str){
+    this.#value = this.#value + str;
+    this.#value = str + this.#value;
+   }
 
-// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
-
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+}
 
 
 
-const sortByDescendingFriendCount = (users) => {
-return users.toSorted((a, b) => b.friends.length - a.friends.length);
-};
-
-
-
-
-console.log(
-    sortByDescendingFriendCount([
-      {
-        name: "Moore Hensley",
-        friends: ["Sharron Pace"],
-        gender: "male"
-      },
-      {
-        name: "Sharlene Bush",
-        friends: ["Briana Decker", "Sharron Pace"],
-        gender: "female"
-      },
-      {
-        name: "Ross Vazquez",
-        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-        gender: "male"
-      },
-      {
-        name: "Elma Head",
-        friends: ["Goldie Gentry", "Aisha Tran"],
-        gender: "female"
-      },
-      {
-        name: "Carey Barr",
-        friends: ["Jordan Sampson", "Eddie Strong"],
-        gender: "male"
-      },
-      {
-        name: "Blackburn Dotson",
-        friends: ["Jacklyn Lucas", "Linda Chapman"],
-        gender: "male"
-      },
-      {
-        name: "Sheree Anthony",
-        friends: ["Goldie Gentry", "Briana Decker"],
-        gender: "female"
-      }
-    ])
-  );
-  
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
