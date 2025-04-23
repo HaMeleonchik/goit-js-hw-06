0fcb3c5ae80fdc6b778cf1da2fce239a3ba5f4cc
HaMeleonchik <tntshow.tiktok@gmail.com>
2025-04-23 20:57:25 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,22 @@
 class StringBuilder {
-#value 
+  #value;
 
-constructor (initialValue){
-this.#value = initialValue
-}
-getValue(){
- return this.#value;
-}
-    padEnd(str) {
-    this.#value = this.#value + str ;
-   }
-   padStart(str){
-    this.#value = str + this.#value;
-   }
-   padBoth(str){
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
     this.#value = this.#value + str;
+  }
+  padStart(str) {
     this.#value = str + this.#value;
-   }
-
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
-
-
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
